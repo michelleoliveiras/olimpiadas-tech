58a138acd5ebf25d8decbcb5f3c02535c9e0770c
michelleoliveiras <56163228+michelleoliveiras@users.noreply.github.com>
2019-12-03 20:30:54 -0300

    Tela do placar de futebol

--- olimpiadasTech/app/src/main/java/com/michelle/olimpiadastech/futebol.kt
+++ olimpiadasTech/app/src/main/java/com/michelle/olimpiadastech/futebol.kt
@@ -2,6 +2,7 @@ package com.michelle.olimpiadastech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_futebol.*
 
 class futebol : AppCompatActivity() {
 
@@ -9,6 +10,42 @@ class futebol : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_futebol)
 
+        pontuacao1.text="0"
+        pontuacao2.text="0"
 
+        bloco1.setOnClickListener {
+            val valorInicial = pontuacao1.text.toString()
+            val novoValor = valorInicial.toInt() + 1
+            pontuacao1.text = novoValor.toString()
+        }
+
+        bloco2.setOnClickListener {
+            val valorInicia2 = pontuacao2.text.toString()
+            val novoValor = valorInicia2.toInt() + 1
+            pontuacao2.text = novoValor.toString()
+        }
+
+        zerarBtn.setOnClickListener{
+            pontuacao1.text = "0"
+            pontuacao2.text = "0"
+        }
+
+        menosTimeABtn.setOnClickListener{
+            val valorInicial = pontuacao1.text.toString()
+            val novoValor = valorInicial.toInt() - 1
+            pontuacao1.text = novoValor.toString()
+
+            if (novoValor <= 0)
+                pontuacao1.text="0"
+        }
+
+        menosTimeBBtn.setOnClickListener{
+            val valorInicial = pontuacao2.text.toString()
+            val novoValor = valorInicial.toInt() - 1
+            pontuacao2.text = novoValor.toString()
+
+            if (novoValor <= 0)
+                pontuacao2.text="0"
+        }
     }
 }
